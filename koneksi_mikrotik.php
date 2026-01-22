4277760186e979d349ef9bbfdc2652c9b6a9cc17
marid4 <75110257+marid4@users.noreply.github.com>
2026-01-22 14:52:05 +0700

    Update MikroTik connection credentials

--- koneksi_mikrotik.php
+++ koneksi_mikrotik.php
@@ -1,9 +1,9 @@
 <?php
 require('routeros_api.class.php');
 
-$ip_mikrotik   = "sin3.tunnel.id:3157"; // Sesuaikan IP Router (Gateway)
-$user_mikrotik = "AwraNet";        // User login Winbox
-$pass_mikrotik = "miekoclok";  // Password login Winbox
+$ip_mikrotik   = "192.168.88.1"; // Sesuaikan IP Router (Gateway)
+$user_mikrotik = "admin";        // User login Winbox
+$pass_mikrotik = "";  // Password login Winbox
 
 $API = new RouterosAPI();
 $API->debug = false; // Ubah ke true jika ingin melihat error log
